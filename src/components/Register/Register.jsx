ad8e93dee1c41e7a703326967250990f015f3b1f
AhmedLotfy8 <ahmedfert45@gmail.com>
2024-08-15 17:12:45 +0200

    Second commit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -15,8 +15,49 @@ export default function Register() {
 
   return (
     <>
-      <h1>Register</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <div className='container max-w-6xl mx-auto  py-8'>
+
+        <div className='flex flex-col'>
+          <h1 className='font-normal dark:text-white'>Register now: </h1>
+          <div className='py-6'>
+            <form class="  mx-auto">
+
+              <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+              </div>
+
+              <div class="mb-5">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+              </div>
+
+              <div class="mb-5">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+              </div>
+
+              <div class="mb-5">
+                <label for="RePassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Re-password</label>
+                <input type="password" id="RePassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+              </div>
+
+              <div class="mb-5">
+                <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone</label>
+                <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+              </div>
+
+            </form>
+
+            <div className='flex justify-end'>
+              <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Register now</button>
+            </div>
+
+          </div>
+        </div>
+
+
+      </div>
     </>
   )
 }
